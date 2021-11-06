b1955e57dc00bff6556d1e704fb7b35123979bf1
IamhappyXD <50207701+IamhappyXD@users.noreply.github.com>
2021-11-07 00:20:58 +0600

    Create generatingSamples.sql
    samples

---
+++ database/DataCreation/generatingSamples.sql
@@ -0,0 +1,86 @@
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('kbeeton0', 'kbeeton0@loc.gov', 'xg9uBp26rGHI', 'Kary Beeton', 11);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('emarusyak1', 'emarusyak1@ask.com', 'nzDqurNzq', 'Ethel Marusyak', 1929);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('acaudelier2', 'acaudelier2@senate.gov', 'IkUZ3cl', 'Alyda Caudelier', 2115);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('gstollwerck3', 'gstollwerck3@salon.com', 'WcLccFa', 'Gerard Stollwerck', 2013);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('eegel4', 'eegel4@tinypic.com', 'ZRlI7ueG', 'Emeline Egel', 2126);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('jdrohun5', 'jdrohun5@histats.com', 'rO6LL5DeVA', 'Joella Drohun', 2708);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('cbartholomaus6', 'cbartholomaus6@home.pl', 'svlc5HS', 'Claudetta Bartholomaus', 2200);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('fcochern7', 'fcochern7@harvard.edu', 'TqX8jad', 'Fanni Cochern', 1934);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('dfidelli8', 'dfidelli8@loc.gov', 'R3DeNmOoXYet', 'Denna Fidelli', 2867);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('hmaclardie9', 'hmaclardie9@tmall.com', '5aDtb50LwNd', 'Hiram MacLardie', 842);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('imathesona', 'imathesona@edublogs.org', '9r47Jt1', 'Isidor Matheson', 1587);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('gjuanicob', 'gjuanicob@storify.com', 'gIZ8TQ1eq9l', 'Gelya Juanico', 1521);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('psatchc', 'psatchc@ihg.com', '6pfHf3j', 'Prinz Satch', 764);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('mlesaunierd', 'mlesaunierd@de.vu', '0ohqei', 'Malanie Lesaunier', 1555);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('hbartuneke', 'hbartuneke@icq.com', 'ut4joUnC', 'Helen-elizabeth Bartunek', 1025);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('hreuterf', 'hreuterf@godaddy.com', 'DANcMu', 'Henderson Reuter', 228);
+insert into administrator (username, user_email, password, admin_name, permission_level) values ('lprescotg', 'lprescotg@cbc.ca', 'zJ6UHE', 'Lyndsie Prescot', 1198);
+
+--- company starts----------
+
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('ndulwich0', 'icicchillo0@va.gov', 'SdW4yZ', 'Vipe', 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', 'Fuxing', '244 Melby Crossing');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('rjosskoviz1', 'jashworth1@oaic.gov.au', 'tYNOwPFi', 'Thoughtbeat', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.
+
+Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 'Horní Stropnice', '80795 Becker Street');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('mhaughin2', 'akillford2@storify.com', 'TwhTKgdPIN7i', 'Eazzy', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
+
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 'Xinxing', '0460 Hoepker Court');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('wsallan3', 'cbewshaw3@vk.com', 'OqcTebmZ', 'Realpoint', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 'Baláo', '7482 Calypso Avenue');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('idunsmuir4', 'mpetrovic4@photobucket.com', '5hHIp8fjt8', 'Linkbridge', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 'Fushan', '4709 Grasskamp Parkway');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('bmackey5', 'sloveland5@stumbleupon.com', 'PWgx7M', 'Tagchat', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
+
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', 'Aco', '05962 Commercial Plaza');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('moddie6', 'bcookes6@fda.gov', 'pdKAitx', 'Demivee', 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 'Chaoshui', '46454 Hansons Plaza');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('clapham7', 'rplumridege7@answers.com', 'HgNeTmY', 'LiveZ', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Mgachi', '118 Delaware Street');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('nklaiser8', 'rsousa8@dagondesign.com', 'cmVdwH', 'Ozu', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.', 'Golacir', '0 Caliangt Parkway');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('sscamwell9', 'ngouly9@sphinn.com', 'hEgdyi3yTKkl', 'Skyba', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.
+
+Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 'Xitou', '91 Dottie Street');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('gdavidovitcha', 'ahartoppa@tumblr.com', 'P3Hn5YC', 'Skimia', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 'Tinampo', '12671 Farragut Avenue');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('jpindellb', 'rbettleyb@comsenz.com', 'DMT6w5gz8zLU', 'Devpulse', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+
+Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 'Lurut', '36265 Delladonna Pass');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('lhebburnc', 'fjeaffersonc@techcrunch.com', 'WHRGQod', 'Jabbercube', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 'Guabito', '504 Vidon Street');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('tcisard', 'bdunckleyd@webeden.co.uk', 'HON13kz3g', 'Tagopia', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 'Shanggan', '1118 Toban Center');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('apollendinee', 'lbryere@liveinternet.ru', '81Z1ztcU', 'Topdrive', 'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', 'Sepulu', '2805 Vahlen Street');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('cpaolinif', 'escoffhamf@cnbc.com', 'AsyaAv3Yv5s', 'Vinder', 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 'Yanjiang', '19 School Court');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('jslanyg', 'jjanouchg@state.gov', 'tcJFC76Ma', 'Jabberbean', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
+
+Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.', 'Baizhong', '3 International Point');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('mbaradelh', 'drosendallh@goo.gl', 'oBYwC8qlHZJq', 'Dazzlesphere', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'Dulce Nombre de Culmí', '63 Linden Circle');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('rmarshi', 'marmandi@chicagotribune.com', 'REQeod', 'Gabtype', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.', 'Avignon', '8 Elka Alley');
+insert into company (username, user_email, password, company_name, description, city_name, address) values ('cburragej', 'gsandhillj@mysql.com', 'MlFkkz8jAi8A', 'Rhycero', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 'Grzmiąca', '4968 Anniversary Drive');
+
+
+--- company ends----------
+
+--- intern starts ---------
+
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('dlaughrey0', 'dlaughrey0@artisteer.com', 'Ylz2XJHkNRT0', 'Dalenna', 'Laughrey', 'University of Basrah', 'Analog Circuit Design manager', 'Agender', 2.34);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('iwoodroffe1', 'iwoodroffe1@mail.ru', 'Re7HQEt', 'Igor', 'Woodroffe', 'Université de Bandundu Ville', 'Human Resources Manager', 'Polygender', 3.76);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('owakely2', 'owakely2@istockphoto.com', 'tmSj5TBo', 'Olympie', 'Wakely', 'Bilkent University', 'Accountant III', 'Male', 4.00);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('rduffan3', 'rduffan3@theguardian.com', 'Bi5y8eR', 'Rosa', 'Duffan', 'The Corcoran College of Art', 'Web Developer III', 'Male', 1.8);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('rgilbert4', 'rgilbert4@wikimedia.org', 'JO3d5Kxa73', 'Rosana', 'Gilbert', 'Yelets State University', 'Recruiter', 'Agender', 2.94);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('awoollett5', 'awoollett5@hibu.com', 'Z0AW2f', 'Arther', 'Woollett', 'University of Greenwich', 'Developer III', 'Genderfluid', 2.76);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('acutchie6', 'acutchie6@gravatar.com', 'DMaPufVSh0I', 'Austin', 'Cutchie', 'Touro College', 'Information Systems Manager', 'Non-binary', 4.0);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('cdrei7', 'cdrei7@freewebs.com', 'lDSXa8n8QF', 'Clemmy', 'Drei', 'Utah Valley State College', 'Assistant Manager', 'Non-binary', 4.0);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('kclotworthy8', 'kclotworthy8@jalbum.net', '2W9hq3FN', 'Kaspar', 'Clotworthy', 'Chengdu Institute of Sichuan International Studies University', 'Assistant Manager', 'Agender', 3.6);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('emcilvaney9', 'emcilvaney9@globo.com', 'tNHLTquPr33', 'Ebonee', 'McIlvaney', 'Indian School of Business Management and Administration', 'Paralegal', 'Genderqueer', 1.5);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('fyarrowa', 'fyarrowa@ucoz.ru', 'gnU0LaUnLf9', 'Flinn', 'Yarrow', 'Stamford University', 'Structural Analysis Engineer', 'Genderqueer', 2.99);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('dmartelb', 'dmartelb@japanpost.jp', 'Xly3Ez', 'Debbie', 'Martel', 'Cork Institute of Technology', 'Paralegal', 'Female', 4.0);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('kgordonc', 'kgordonc@macromedia.com', 'sR8EePRSAyH', 'Kendall', 'Gordon', 'Gomal University', 'Business Systems Development Analyst', 'Genderfluid', 3.45);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('mpapachristophoroud', 'mpapachristophoroud@is.gd', '6D3eLsYOJnY', 'Mikel', 'Papachristophorou', 'Yorker International University, Athens', 'Recruiter', 'Agender', 1.94);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('prultone', 'prultone@qq.com', 'Zxrmi8rQ', 'Pascal', 'Rulton', 'Universiti Malaysia Kelantan', 'Software Engineer III', 'Bigender', 4.00);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('cdelucef', 'cdelucef@purevolume.com', 'HQuQqFZ', 'Chad', 'Deluce', 'Claremont Lincoln University', 'Budget/Accounting Analyst II', 'Non-binary', 1.98);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('cgrocottg', 'cgrocottg@youtu.be', 'kbR2rfRK', 'Cecilio', 'Grocott', 'Universidad Hispanoamericana', 'Senior Financial Analyst', 'Genderfluid', 3.46);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('ngoodersh', 'ngoodersh@va.gov', 'mPBcTrwd', 'Nara', 'Gooders', 'Universitas Brawijaya', 'VP Marketing', 'Non-binary', 2.78);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('aberii', 'aberii@businesswire.com', '6gPSv7', 'Aurora', 'Beri', 'Private FernFachhochschule Darmstadt', 'Teacher', 'Genderfluid', 4.0);
+insert into intern (username, user_email, password, first_name, last_name, university_name, major, gender, cGpa) values ('tbeafordj', 'tbeafordj@purevolume.com', 'LOxs6F', 'Theodore', 'Beaford', 'Modern Sciences & Arts University', 'Software Test Engineer III', 'Non-binary', 3.45);
+
+--- intern ends ---------
